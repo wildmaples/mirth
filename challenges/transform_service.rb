b8aa4394e9c1111423ae7aeda3613f4cc8b80920
Maple Ong <maple.ong@shopify.com>
2020-11-11 11:12:17 -0500

    Add single threaded capitalization service

---
+++ challenges/transform_service.rb
@@ -0,0 +1,27 @@
+require 'socket'
+
+class TransformService
+  def initialize(port)
+    server = TCPServer.new(port)
+
+    loop do
+      client = server.accept
+
+      while input = client.gets
+        puts "Received #{input.chomp} from a client socket on #{port}"
+        client.puts capitalize(input)
+      end
+
+      puts "Closing client socket"
+      client.close
+    end
+  end
+
+  private
+
+  def capitalize(text)
+    text.upcase
+  end
+end
+
+TransformService.new(1234)
