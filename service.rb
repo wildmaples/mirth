93bc79bf1e8165c952a33dc92b5b3a806f825dc9
Maple Ong <maple.ong@shopify.com>
2020-11-28 22:04:13 -0500

    Add default http response to any clients

--- service.rb
+++ service.rb
@@ -8,10 +8,28 @@ class Service
       client = server.accept
       request_line = client.readline
       method_token, target, version_number = request_line.split
+      response_message =  "✅ Received a #{method_token} request to #{target} with #{version_number} on #{port}"
 
-      puts "✅ Received a #{method_token} request to #{target} with #{version_number} on #{port}"
+      puts response_message
+
+      default_http_response = <<~MSG
+        #{version_number} 201 OK
+        #{headers.join("\n")}
+
+        #{response_message}
+      MSG
+
+      client.puts default_http_response
+
       puts "🛑 Closing client socket"
+
       client.close
     end
+  end
+
+  def headers
+    [
+      "Content-Type: text/plain",
+    ]
   end
 end
