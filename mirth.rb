dcf982651be1273437be58eaded0410e9c896ce1
Maple Ong <maple.ong@shopify.com>
2021-02-11 12:14:27 -0500

    Establish connection to ActiveRecord database

--- mirth.rb
+++ mirth.rb
@@ -1,7 +1,10 @@
 require_relative 'service_app'
 require 'rack/handler/puma'
+require 'active_record'
 
 $stdout.sync = true # Turn on auto-flushing
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "mirth.sqlite3")
 
 app = ServiceApp::Mirth
 
