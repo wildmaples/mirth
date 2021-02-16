b6b0b8e492b0e078cc5dace2ed2ba2ecf7fedf0a
Maple Ong <maple.ong@shopify.com>
2021-02-16 14:02:13 -0500

    Remove old code

--- mirth.rb
+++ mirth.rb
@@ -59,31 +59,4 @@ router.draw do
   end
 end 
 
-# app = -> environment {
-#   request = Rack::Request.new(environment)
-#   response = Rack::Response.new
-  
-#   if request.get? && request.path == "/show/data"
-#     response.write("<ul>\n")
-#     response.content_type = "text/html; charset=UTF-8"
-    
-#     DailyData.all.each do |daily_data|
-#       response.write "<li> On this day <b>#{CGI.escapeHTML(daily_data.date)}</b>, #{CGI.escapeHTML(daily_data.step_count.to_s)}, #{CGI.escapeHTML(daily_data.notes)}</li>\n"
-#     end
-
-#     response.write "</ul>\n"
-#     response.write daily_data_form
-
-#   elsif request.post? && request.path == "/add/data"
-#     params = request.params
-#     DailyData.create(date: params['date'], step_count: params["step_count"], notes: params["notes"])
-#     response.redirect('/show/data', 303)
-#   else
-#     response.content_type = "text/plain; charset=UTF-8"
-#     response.write("✅ Received a #{request.request_method} request to #{request.path}!")
-#   end
-      
-#   response.finish
-# }
-
 Rack::Handler::Puma.run(router, :Port => 1234, :Verbose => true)
